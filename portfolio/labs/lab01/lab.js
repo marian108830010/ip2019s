ac7094a023b0b20ceff8d67ec48f5c6cf83980b5
marian108830010 <52520167+marian108830010@users.noreply.github.com>
2019-07-07 19:15:59 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,7 +4,36 @@
 
 
 function start(e) {
+    var name = [
+        {
+            char: '魏',
+            big5: 'C351',
+            cns: '1-777B',
+            unicode: '9B4F'
+        },
+        {   char: '敏',
+            big5: 'B1D3',
+            cns: '1-5A77',
+            unicode: '654F'
+        },
+        {   char: '安',
+            big5: 'A677',
+            cns: '1-477B',
+            unicode: '5B89'
+        },
+    ];
+    var ele = document.getElementById('div1');
 
+    for (var i = 0; i < 3; i++){
+        ele.innerHTML += name[i].char;
+        ele.innerHTML += ' big5 = ';
+        ele.innerHTML += name[i].big5;
+        ele.innerHTML += ' cns = ';
+        ele.innerHTML += name[i].cns;
+        ele.innerHTML += ' unicode = ';
+        ele.innerHTML += name[i].unicode;
+        ele.innerHTML += '<br>';
+    }
 }
 
 /*
