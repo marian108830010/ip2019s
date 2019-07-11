14f83868f97a8ae7baa7b867be15006bb743dcd8
marian108830010 <52520167+marian108830010@users.noreply.github.com>
2019-07-11 12:23:37 +0800

    Add files via upload

--- portfolio/labs/lab04/lab.js
+++ portfolio/labs/lab04/lab.js
@@ -1,13 +1,26 @@
 
-function issueCard() {
-    
-}
-
-function update() {
-    
-}
 
 $(function() {
-  
+    var card = [ 'ace-of-club.svg',
+                 'ace-of-diamonds.svg',
+                 'ace-of-hearts.svg',
+                 'ace-of-spades.svg',
+                 'joker-card.svg'];
+                
+                for (var i = 0; i < 16; i++) {
+                    var r = Math.floor((Math.random() * 4 ));
+                    $('#d2').append('<img class="issue" src="./' + card[r] + '">');
+                }
+
+                $('#b1').on('click', function() {
+                //code to happen on click.
+                $('issue').hide();
+                $('#d2').html('');
+
+                for (var i = 0; i < 16; i++){
+                $('#d2').append('<img src="./joker-card.svg">');
+                }
+                });
+
 });
 
